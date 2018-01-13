f0152b5212d8482e24c59af353c42ebe2351bd44
Judy McGoldrick <jjeanius@verizon.net>
2018-01-13 22:14:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister                # describe 'CashRegister
         items << title
 
       end
-      last_transaction=@total
+      @last_transaction = @total
     end
 
     def apply_discount
@@ -29,7 +29,7 @@ class CashRegister                # describe 'CashRegister
 
 
     def void_last_transaction
-      @total - last_transaction
+      @total -= last_transaction
     end
 
   end
